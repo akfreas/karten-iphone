7fd4feb1022a6c9c35f1842a7ee445b3ab9cf652
Alex Freas <akfreas@gmail.com>
2014-03-09 07:19:13 +0100

    Initial Commit

---
+++ Karten/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Karten
+//
+//  Created by Alexander Freas on 3/9/14.
+//  Copyright (c) 2014 Sashimiblade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ KartenTests/KartenTests.m
@@ -0,0 +1,34 @@
+//
+//  KartenTests.m
+//  KartenTests
+//
+//  Created by Alexander Freas on 3/9/14.
+//  Copyright (c) 2014 Sashimiblade. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KartenTests : XCTestCase
+
+@end
+
+@implementation KartenTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
