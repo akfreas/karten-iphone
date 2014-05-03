201d0aa6d726417478776046727b735bb2ccdb6f
Alex Freas <akfreas@gmail.com>
2014-05-03 19:13:59 +0200

    [card model] Now maps to core data and has couchdb related properties.

--- Karten/Classes/Card.h
+++ Karten/Classes/Card.h
@@ -1,8 +1,21 @@
-@interface Card : NSObject
+//
+//  Card.h
+//  Karten
+//
+//  Created by Alexander Freas on 4/22/14.
+//  Copyright (c) 2014 Sashimiblade. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Card : NSManagedObject
 
 @property (nonatomic, retain) NSString * term;
 @property (nonatomic, retain) NSString * definition;
-
-
+@property (nonatomic, retain) NSNumber * knowledgeScore;
+@property (nonatomic, retain) NSString * couchID;
+@property (nonatomic, retain) NSString * couchRev;
 
 @end
--- Karten/Classes/Card.m
+++ Karten/Classes/Card.m
@@ -1,6 +1,20 @@
+//
+//  Card.m
+//  Karten
+//
+//  Created by Alexander Freas on 4/22/14.
+//  Copyright (c) 2014 Sashimiblade. All rights reserved.
+//
+
 #import "Card.h"
 
 
 @implementation Card
+
+@dynamic term;
+@dynamic definition;
+@dynamic knowledgeScore;
+@dynamic couchID;
+@dynamic couchRev;
 
 @end
