dea0d3da2c2f46501faa5c58c40f90b9504b57e7
Alex Freas <akfreas@gmail.com>
2014-05-03 19:12:04 +0200

    [card helpers] Added functionality to copy couch db data to core data.

--- Karten/Classes/Card+Helpers.h
+++ Karten/Classes/Card+Helpers.h
@@ -1,5 +1,8 @@
 #import "Card.h"
-
+#import <CoreData/CoreData.h>
 @interface Card (Helpers)
+
++ (instancetype)getOrCreateCardWithCouchDBQueryRow:(CBLQueryRow *)row inContext:(NSManagedObjectContext *)context;
++ (NSArray *)allCards;
 
 @end
--- Karten/Classes/Card+Helpers.m
+++ Karten/Classes/Card+Helpers.m
@@ -1,5 +1,49 @@
 #import "Card+Helpers.h"
 
+static NSString *kCouchIDAttr = @"couchID";
+static NSString *kCouchRevAttr = @"couchRev";
+static NSString *kCouchDefinitionKey = @"definition";
+static NSString *kCouchTermKey = @"term";
+static NSString *kCouchIDKey = @"_id";
+//static NSString *kCouchRevKey = @"_re
+
 @implementation Card (Helpers)
++ (instancetype)getOrCreateCardWithCouchDBQueryRow:(CBLQueryRow *)row inContext:(NSManagedObjectContext *)context
+{
+    
+    NSPredicate *pred = [NSPredicate predicateWithFormat:@"couchID == %@", row.document.documentID];
+    
+    NSArray *cardArray = [Card MR_findAllWithPredicate:pred inContext:context];
+    Card *card;
+    if ([cardArray count] == 1) {
+        card = [cardArray firstObject];
+        if ([card.couchRev isEqualToString:row.documentRevisionID] == NO) {
+            [card updateWithRow:row];
+        }
+    } else if ([cardArray count] == 0) {
+        card = [Card newCardWithQueryRow:row inContext:context];
+    }
+    return card;
+}
+
++ (instancetype)newCardWithQueryRow:(CBLQueryRow *)row inContext:(NSManagedObjectContext *)context
+{
+    Card *newCard = [Card MR_createInContext:context];
+    [newCard updateWithRow:row];
+    return newCard;
+}
+
+- (void)updateWithRow:(CBLQueryRow *)row
+{
+    self.definition = row.document[kCouchDefinitionKey];
+    self.term = row.document[kCouchTermKey];
+    self.couchID = row.documentID;
+    self.couchRev = row.documentRevisionID;
+    self.knowledgeScore = @(0);
+}
+
+//+ (NSArray *)allCards {
+////    [self
+//}
 
 @end
